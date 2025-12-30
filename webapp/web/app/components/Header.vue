bc391754db62fbadc88cfe562705b55c24d8b99a
keroro0402 <otsuyu0402@yahoo.co.jp>
2025-12-30 15:40:44 +0900

    共通ヘッダー作成

---
+++ webapp/web/app/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+    </nav>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped style lang="scss">
+.header {
+  background-color: #2c3e50;
+  padding: 1rem;
+  color: white;
+}
+nav a {
+  margin-right: 1rem;
+  color: white;
+  text-decoration: none;
+}
+</style>
---
+++ webapp/web/app/pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Index page</h1>
+  <!-- <NuxtLink to="/blog/hello-world"> Go to blog post </NuxtLink> -->
+</template>
