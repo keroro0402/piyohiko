bbc58847c1ab3d408b6ea77395c157dd796c9603
keroro0402 <otsuyu0402@yahoo.co.jp>
2025-12-31 00:29:52 +0900

    naviのリンクを参照型に修正

--- webapp/web/app/components/Navi.vue
+++ webapp/web/app/components/Navi.vue
@@ -1,5 +1,9 @@
 <template>
   <nav>
-    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink v-for="link in links.navi" :key="link.key" :to="link.to">{{ link.label }}</NuxtLink>
   </nav>
 </template>
+
+<script setup>
+import { links } from '~/constants/links.ts';
+</script>
