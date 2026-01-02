da2881f021dc761f70e7573cd47c29eed795fed3
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-01-02 22:20:04 +0900

    画像反映

--- webapp/web/app/pages/index.vue
+++ webapp/web/app/pages/index.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Index page</h1>
+  <main>
+    <h1>Index page</h1>
+    <img v-for="image in images.main" :key="image.key" :src="image.path" :loading="image.loading" />
+  </main>
 </template>
+
+<script setup lang="ts">
+import { images } from '~/constants/images';
+</script>
