9c0bc1a037eda72ac0e40a6a74eaed209f61cfee
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-01-02 21:26:53 +0900

    フッターにナビを反映させた

--- webapp/web/app/components/Footer.vue
+++ webapp/web/app/components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
   <footer class="footer">
-    <nav>
-      <NuxtLink to="/">Home</NuxtLink>
-    </nav>
+    <Navi />
   </footer>
 </template>
+
+<script setup>
+import Navi from '~/components/Navi.vue';
+</script>
