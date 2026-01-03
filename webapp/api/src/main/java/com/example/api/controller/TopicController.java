aa210ec6d77cf2cef8b9de1aaf4f5536f6cf28a3
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-01-03 15:59:25 +0900

    トピック用コントローラ作成

---
+++ webapp/api/src/main/java/com/example/api/controller/TopicController.java
@@ -0,0 +1,28 @@
+package com.example.api.controller;
+
+import com.example.api.form.TopicRegistrationForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class TopicController {
+    /* Topic登録画面表示 */
+    @GetMapping("/show-topic-form")
+    public String showTopicForm(){
+        return "register-topic";
+    }
+
+    /* Topic登録リクエスト（Topic登録画面から遷移） */
+    @PostMapping("/register-topic")
+    /*
+    * @ModelAttribute TopicRegistrationForm
+    * 入力された情報を自動でTopicRegistrationFormのフィールドにマッピング（データバインディング）されてformに格納される
+    * HTMLの <input name="topicTitle"> → TopicRegistrationForm.topicTitle に自動でセット
+    * */
+    public String registerTopic(@ModelAttribute TopicRegistrationForm formInfo){
+        System.out.println(formInfo); /* 中身を確認 */
+        return "confirm-register-topic";
+    }
+}
