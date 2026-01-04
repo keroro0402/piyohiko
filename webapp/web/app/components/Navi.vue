37858a8ec54193bddbb623cceabe0a577609037a
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-01-04 23:24:41 +0900

    navにクラス名付与（propsを使用）

--- webapp/web/app/components/Navi.vue
+++ webapp/web/app/components/Navi.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav>
+  <nav :class="block">
     <NuxtLink v-for="link in links.navi" :key="link.key" :to="link.to" :class="{ current: isCurrent(link) }">{{ link.label }}</NuxtLink>
   </nav>
 </template>
@@ -8,6 +8,11 @@
 import { links } from '~/constants/links';
 import { useRoute } from 'vue-router';
 import type { NaviItem } from '~/types/navi';
+
+/* props 定義*/
+const { block } = defineProps<{
+  block: string;
+}>();
 
 /* route */
 const route = useRoute();
