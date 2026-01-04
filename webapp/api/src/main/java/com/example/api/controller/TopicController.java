24764c2b04e526ebb3b72d97da54683dead28914
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-01-04 15:17:17 +0900

    モデル追加

--- webapp/api/src/main/java/com/example/api/controller/TopicController.java
+++ webapp/api/src/main/java/com/example/api/controller/TopicController.java
@@ -2,6 +2,7 @@ package com.example.api.controller;
 
 import com.example.api.form.TopicRegistrationForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,7 +11,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class TopicController {
     /* Topic登録画面表示 */
     @GetMapping("/show-topic-form")
-    public String showTopicForm(){
+    public String showTopicForm(Model model){
+        model.addAttribute("templateTitle", "トピック登録");
+        model.addAttribute("templateRegistrationId", "【登録ID】");
+        model.addAttribute("templateUserId", "【ユーザーID】");
+        model.addAttribute("templateVisitDate", "【登録日時】");
+        model.addAttribute("templateTopicTitle", "【トピックタイトル】");
+        model.addAttribute("templateTopicContent", "【内容】");
+        model.addAttribute("templateRegister", "登録する");
         return "register-topic";
     }
 
@@ -21,7 +29,14 @@ public class TopicController {
     * 入力された情報を自動で TopicRegistrationForm のフィールドにマッピング（データバインディング）されて formInfo に格納される
     * HTMLの <input name="topicTitle"> → TopicRegistrationForm.topicTitle に自動でセット
     * */
-    public String registerTopic(@ModelAttribute TopicRegistrationForm formInfo){
+    public String registerTopic(@ModelAttribute TopicRegistrationForm formInfo, Model model){
+        model.addAttribute("templateTitle", "トピック登録内容確認");
+        model.addAttribute("templateRegistrationId", "【登録ID】");
+        model.addAttribute("templateUserId", "【ユーザーID】");
+        model.addAttribute("templateVisitDate", "【登録日時】");
+        model.addAttribute("templateTopicTitle", "【トピックタイトル】");
+        model.addAttribute("templateTopicContent", "【内容】");
+        model.addAttribute("templateToRegister", "入力ページへ");
         System.out.println(formInfo); /* 変数 formInfo の中身を確認 */
         return "confirm-register-topic";
     }
