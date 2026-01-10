44a29bc8ed27e228ffea4b0f793b59a0c2012303
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-01-11 00:21:30 +0900

    RegisterServiceImpl で RegisterService を実装

--- webapp/api/src/main/java/com/example/api/service/RegisterServiceImpl.java
+++ webapp/api/src/main/java/com/example/api/service/RegisterServiceImpl.java
@@ -1,4 +1,8 @@
 package com.example.api.service;
 
-public class RegisterServiceImpl {
+public class RegisterServiceImpl implements RegisterService {
+    @Override
+    public String register() {
+        return "トピック登録完了!!!!";
+    }
 }
