6c2168b889ab9e14fae627e67525d9925edddb26
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-01-27 01:01:40 +0900

    サービス追加、サービスのDI追加

---
+++ webapp/api/src/main/java/com/example/api/service/RegisterServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.api.service;
+
+import com.example.api.entity.Review;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisterServiceImpl implements RegisterService {
+    @Override
+    public void register(Review review){
+
+    }
+}
