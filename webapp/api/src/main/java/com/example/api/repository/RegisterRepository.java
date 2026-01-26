98036e83f587b79a3e6007032bf9a434f3e8482b
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-01-27 01:35:54 +0900

    インフラ層（repositry）追加

---
+++ webapp/api/src/main/java/com/example/api/repository/RegisterRepository.java
@@ -0,0 +1,7 @@
+package com.example.api.repository;
+
+import com.example.api.entity.Review;
+
+public interface RegisterRepository {
+    void add(Review review);
+}
--- webapp/api/src/main/java/com/example/api/service/RegisterServiceImpl.java
+++ webapp/api/src/main/java/com/example/api/service/RegisterServiceImpl.java
@@ -1,12 +1,18 @@
 package com.example.api.service;
 
 import com.example.api.entity.Review;
+import com.example.api.repository.RegisterRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RegisterServiceImpl implements RegisterService {
+
+    private final RegisterRepository repository;
+
     @Override
     public void register(Review review){
-
+        repository.add(review);
     }
 }
