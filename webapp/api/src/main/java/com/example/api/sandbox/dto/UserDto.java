b17c06ea0b042b6f85869b8df1c47320362d01e6
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-01-27 15:18:26 +0900

    sandbox_DTO / Service / Controller / AllArgsConstructor / NoArgsConstructor / setter / JSON 受け取り返却の演習

---
+++ webapp/api/src/main/java/com/example/api/sandbox/dto/UserDto.java
@@ -0,0 +1,14 @@
+package com.example.api.sandbox.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private int id;
+    private String name;
+    private int age;
+}
