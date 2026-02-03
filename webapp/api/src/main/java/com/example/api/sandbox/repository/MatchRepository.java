436ce0c67804416c9151b0a777689842e08a371a
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-02-03 23:14:18 +0900

    sandbox_インフラ層のメモ追加

--- webapp/api/src/main/java/com/example/api/sandbox/repository/MatchRepository.java
+++ webapp/api/src/main/java/com/example/api/sandbox/repository/MatchRepository.java
@@ -4,6 +4,13 @@ import java.time.LocalDate;
 
 
 public interface MatchRepository {
-    void add(MatchDto dto);
+    /* MatchRepogitoryImplで使用する抽象メソッドを定義*/
+    void add(MatchDto dto); // DBにdtoを登録するメソッド
+
+    /*インフラ層では重複登録が「ある」「ない」を調査するメソッドを定義する
+    *実際に登録するしないの判断をするメソッドではないのがポイント
+    * リポジトリに書く処理はその結果で、サービスは仕事を進めれるかを念頭にするとよい。
+    * 今回でいえばDBに登録が「ある」「ない」の事実だけを出せばよい
+    * */
     boolean existByDateAndPlace(LocalDate date, String place);
 }
