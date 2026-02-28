d017ad2bfb82e2565b8cc6e113ae4e81cfc8da61
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-03-01 01:10:20 +0900

    テストデータ削除しList<Review> listにfrom.getRestaurantId()の結果を代入に変更

--- webapp/api/src/main/java/com/example/api/controller/ReviewListController.java
+++ webapp/api/src/main/java/com/example/api/controller/ReviewListController.java
@@ -18,33 +18,15 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReviewListController {
 
-    private final ReviewListService reviewListService;
+    private final ReviewListService service;
 
     @PostMapping("/search-review")
     private String serachReview(
             @ModelAttribute ReviewSearchForm from,
             Model model
             ){
-        List<Review> list = new ArrayList<Review>();
-        // テストデータ
-        // 1件目
-        Review review = new Review();
-        review.setReviewId(1);
-        review.setRestaurantId(1);
-        review.setUserId("user1");
-        review.setVisitDate(LocalDate.of(2027,7,1));
-        review.setRating(3);
-        review.setComment("comment1");
-        list.add(review);
-        // 2件目
-        review = new Review();
-        review.setReviewId(2);
-        review.setRestaurantId(1);
-        review.setUserId("user2");
-        review.setVisitDate(LocalDate.of(2027,7,2));
-        review.setRating(4);
-        review.setComment("comment2");
-        list.add(review);
+
+        List<Review> list = service.findByRestaurantId(from.getRestaurantId());
 
         if(list.size() > 0){
             model.addAttribute("reviewList", list);
