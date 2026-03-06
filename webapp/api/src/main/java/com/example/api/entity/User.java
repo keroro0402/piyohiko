a850f3ba1794cb6bcdeac1d7a377cffa967a59c7
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-03-06 23:04:17 +0900

    自作Getter/SetterからLombokの @Getter @Setterに変更

--- webapp/api/src/main/java/com/example/api/entity/User.java
+++ webapp/api/src/main/java/com/example/api/entity/User.java
@@ -1,32 +1,37 @@
 package com.example.api.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
     private Integer userId;
     private String loginId;
     private String password;
 
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public void setLoginId(String loginId){
-        this.loginId = loginId;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-    public Integer getUserId(){
-        return this.userId;
-    }
-
-    public String getLoginId(){
-        return this.loginId;
-    }
-
-    public String getPassword(){
-        return this.password;
-    }
+//    public void setUserId(Integer userId) {
+//        this.userId = userId;
+//    }
+//
+//    public void setLoginId(String loginId){
+//        this.loginId = loginId;
+//    }
+//
+//    public void setPassword(String password){
+//        this.password = password;
+//    }
+//
+//    public Integer getUserId(){
+//        return this.userId;
+//    }
+//
+//    public String getLoginId(){
+//        return this.loginId;
+//    }
+//
+//    public String getPassword(){
+//        return this.password;
+//    }
 
 }
