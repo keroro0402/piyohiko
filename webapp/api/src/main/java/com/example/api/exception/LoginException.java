0b4ba34893341d853c806eb1571e6711fb2a9bf6
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-03-07 23:50:22 +0900

    エラーメッセージの取得方針をハードコーディングから参照する方針に修正

--- webapp/api/src/main/java/com/example/api/exception/LoginException.java
+++ webapp/api/src/main/java/com/example/api/exception/LoginException.java
@@ -1,7 +1,20 @@
 package com.example.api.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginException extends RuntimeException{
-    public LoginException(String message){
+
+    private String errorCode;
+
+    public LoginException(String errorCode, String message){
         super(message);
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
     }
 }
