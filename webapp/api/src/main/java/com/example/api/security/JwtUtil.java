6d701e32269a658713ff8ff5fe99537f5d62bd4f
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-04-04 23:51:40 +0900

    JwtUtilクラスを新規作成

---
+++ webapp/api/src/main/java/com/example/api/security/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.example.api.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+public class JwtUtil {
+    private static final String SECRET_KEY = "my-super-secret-key-that-is-very-long-123412256";
+
+    private static final SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8)); // 文字列の秘密鍵を署名用のSecretKeyに変換
+
+    public static String generateToken(String loginId){
+
+        return Jwts.builder()  // トークンの土台作成
+                .setSubject(loginId)  // ユーザー識別子（Subject）を設定
+                .setIssuedAt(new Date())  // 発行日時（IssuedAt）を設定
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))  // 有効期限（Expiration）を設定
+                .signWith(key)  // 秘密鍵で署名（改ざん防止）
+                .compact();  // JWTを生成して文字列として返す
+    }
+
+    public static String getLoginIdFromToken(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
