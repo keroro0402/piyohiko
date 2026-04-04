cfe36c04cd7b969c280bf0a649bc892af9e4e74e
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-04-05 00:05:23 +0900

    JWT認証フィルタを適用

--- webapp/api/src/main/java/com/example/api/config/SecurityFilterConfig.java
+++ webapp/api/src/main/java/com/example/api/config/SecurityFilterConfig.java
@@ -1,20 +1,31 @@
 package com.example.api.config;
 
+import com.example.api.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityFilterConfig {
+
+    private final JwtFilter jwtFilter;
+    public SecurityFilterConfig(JwtFilter jwtFilter){
+        this.jwtFilter = jwtFilter;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // CSRF（Cross-Site Request Forgery）保護を無効化
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+//                        .anyRequest().permitAll()
+                                .requestMatchers("/login").permitAll()
+                                .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .anonymous(anon -> anon.disable())
                 /*
                 * Spring Security が使う "/login" を使えないようにする(エンドポイントで /login が使えるようにする)
                 * これがないとSecurityFilterChain が /login を使うので Controller で使えない
