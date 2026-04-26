472e40e8a654162533f0f85eba43ce69ecbd0ff4
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-04-26 17:17:52 +0900

    アプリ内テキスト用の定数ファイルを作成

---
+++ webapp/web/app/constants/text.ts
@@ -0,0 +1,14 @@
+/**
+ * アプリ内文言定義
+ *
+ * ルール:
+ * - key は大文字
+ */
+
+export const TEXT = {
+  LOGIN: {
+    LOGINLABEL: 'ログイン',
+    MAILLABEL: 'メールアドレス',
+    PASSWORDLABEL: 'パスワード',
+  },
+} as const;
