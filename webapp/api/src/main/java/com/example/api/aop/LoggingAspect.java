d2d74d9a401804c35b7cec30cf868320f1395d7d
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-05-09 22:18:19 +0900

    LoggingAspectに異常終了時のログ出力を追加し、正常終了時のログ出力を改善

--- webapp/api/src/main/java/com/example/api/aop/LoggingAspect.java
+++ webapp/api/src/main/java/com/example/api/aop/LoggingAspect.java
@@ -2,6 +2,7 @@ package com.example.api.aop;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -12,26 +13,50 @@ import java.time.format.DateTimeFormatter;
 @Aspect
 @Component
 public class LoggingAspect {
-    @Before("execution(* com.example.api.service.*.*(..))")
+    @Before("execution(* com.example.api.service..*Impl.*(..))")
     public void logBefore(JoinPoint joinPoint){
-        outputLog("メソッド開始", joinPoint);
+        String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        outputLog("実行開始メソッド", className, methodName);
     }
 
-    @AfterReturning("execution(* com.example.api.service.*.*(..))")
+    // メソッドが「正常終了」した時のみ実行される後処理ログ
+    @AfterReturning("execution(* com.example.api.service..*Impl.*(..))")
     public void logAfter(JoinPoint joinPoint){
-        outputLog("メソッド終了", joinPoint);
+        String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        outputLog("実行済みメソッド", className, methodName);
     }
-    // 共通ログ出力メソッド
-    private void outputLog(String str, JoinPoint joinPoint){
+
+    // メソッドが「異常終了」した時のみ実行される後処理ログ
+    @AfterThrowing(value = "execution(* com.example.api.service..*Impl.*(..))", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e){
+        String className = joinPoint.getSignature().getDeclaringType().getSimpleName();
+        String methodName = joinPoint.getSignature().getName();
+        outputErrorLog( "エラー：" + e.getMessage(), className, methodName);
+    }
+
+    // 正常終了ログ出力メソッド
+    private void outputLog( String str, String className, String methodName){
+        long threadId = Thread.currentThread().getId();
         // 出力時刻文字列取得
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String strNow = LocalDateTime.now().format(formatter);
-        // クラス名・メソッド名取得
-        String className = joinPoint.getTarget().getClass().getSimpleName();
-        String methodName = joinPoint.getSignature().getName();
         // ログ表示
         System.out.println(
-                strNow + " : " + str + " : " + className + "." + methodName + "()"
+                strNow + " [Thread:" + threadId + "] : " + "クラス" + " : " + className + " , " + str + " : " + methodName + "()"
+        );
+    }
+
+    // 異常終了ログ出力メソッド
+    private void outputErrorLog( String className, String methodName, String error){
+        long threadId = Thread.currentThread().getId();
+        // 出力時刻文字列取得
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String strNow = LocalDateTime.now().format(formatter);
+        // エラーログ表示
+        System.out.println(
+                strNow + " [Thread:" + threadId + "] : " + "クラス" + " : " + className + " , " + "【エラー発生】メソッド" + " : " + methodName + "()"
         );
     }
 
