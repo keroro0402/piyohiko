cb83e11b588b46f872cc9aaec7e22afb49dd7024
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-05-10 22:11:48 +0900

    エラーハンドラーを追加し、ログイン処理でのエラーメッセージ表示を実装

---
+++ webapp/web/app/api/errorHandler.ts
@@ -0,0 +1,23 @@
+import type { AxiosError } from 'axios';
+import { STATES_CODE } from '~/constants/api-status';
+import { TEXT } from '~/constants/text';
+
+const errorHandler = (error: unknown): string => {
+  if (!(error instanceof Error)) return TEXT.ERROR.UNKNOWNERROR;
+
+  const axiosError = error as AxiosError;
+  const status = axiosError.response?.status || STATES_CODE.INTERNAL_SERVER_ERROR;
+
+  switch (status) {
+    case STATES_CODE.UNAUTHORIZED:
+      return TEXT.ERROR.ERROR401;
+    case STATES_CODE.FORBIDDEN:
+      return TEXT.ERROR.ERROR403;
+    case STATES_CODE.INTERNAL_SERVER_ERROR:
+      return TEXT.ERROR.ERROR500;
+    default:
+      return TEXT.ERROR.UNKNOWNERROR;
+  }
+};
+
+export { errorHandler };
