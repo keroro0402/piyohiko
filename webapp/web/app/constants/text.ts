666e4700f529d5961dd6c1601ab62de46decaecd
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-05-24 16:24:53 +0900

    バリデーションに文字数、値、確認用のバリデーション追加 エラーメッセージを定数に定義

--- webapp/web/app/constants/text.ts
+++ webapp/web/app/constants/text.ts
@@ -3,6 +3,7 @@
  *
  * @rules
  * - key は大文字
+ * - 階層 は2階層まで
  */
 export const TEXT = {
   COMMON: {
@@ -18,8 +19,14 @@ export const TEXT = {
     LOGINID: 'ログインID',
     PASSWORD: 'パスワード',
     REQUIRED: '必須項目です',
-    ERROR_REQUIRED_LOGIN_ID: 'ログインIDを入力してください',
-    ERROR_REQUIRED_PASSWORD: 'パスワードを入力してください',
+    ERROR_LOGIN_ID_REQUIRED: 'ログインIDを入力してください',
+    ERROR_PASSWORD_REQUIRED: 'パスワードを入力してください',
+    ERROR_LOGIN_ID_MIN: (loginId_min: number) => `ログインIDは${loginId_min}文字以上で入力してください`,
+    ERROR_LOGIN_ID_MAX: (loginId_max: number) => `ログインIDは${loginId_max}文字以内で入力してください`,
+    ERROR_LOGIN_ID_ALPHANUMERIC: 'ログインIDは半角英数字のみで入力してください',
+    ERROR_PASSWORD_MIN: (password_min: number) => `パスワードは${password_min}文字以上で入力してください`,
+    ERROR_PASSWORD_MAX: (password_max: number) => `パスワードは${password_max}文字以内で入力してください`,
+    ERROR_PASSWORD_ALPHANUMERIC: 'パスワードは半角英数字のみで入力してください',
   },
   LOGIN: {
     LABEL: 'ログイン',
@@ -30,6 +37,7 @@ export const TEXT = {
     LABEL: '新規登録',
     CONFIRM_PASSWORD_LABEL: 'パスワード（確認）',
     ERROR_REQUIRED_CONFIRM_PASSWORD: '確認用パスワードを入力してください',
+    ERROR_PASSWORD_MISMATCH: 'パスワードが一致しません',
   },
   ERROR: {
     ERROR401: 'ログインに失敗しました。IDとパスワードを確認してください。',
