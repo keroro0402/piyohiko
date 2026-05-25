cd6695ad9bfd247bbb55213b945cb6497b0b8f8e
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-05-26 00:21:03 +0900

    アノテーションをControllerからRestControllerに変更

--- webapp/api/src/main/java/com/example/api/controller/RegisterController.java
+++ webapp/api/src/main/java/com/example/api/controller/RegisterController.java
@@ -5,17 +5,13 @@ import com.example.api.entity.Review;
 import com.example.api.form.ReviewRegisterForm;
 import com.example.api.service.RegisterService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class RegisterController {
 
