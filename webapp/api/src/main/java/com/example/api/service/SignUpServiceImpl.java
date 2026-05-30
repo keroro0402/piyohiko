cc60bb2b11f9a48cf1992b85aa2bcadd21ad1a6e
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-05-31 01:28:19 +0900

    passwordのエンコード処理をサービスに追記

--- webapp/api/src/main/java/com/example/api/service/SignUpServiceImpl.java
+++ webapp/api/src/main/java/com/example/api/service/SignUpServiceImpl.java
@@ -1,16 +1,24 @@
 package com.example.api.service;
 
 import com.example.api.form.SignUpForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SignUpServiceImpl implements SignUpService{
+
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     public String signUp(SignUpForm signUpForm){
         System.out.println("*******************");
         System.out.println(signUpForm.getLoginId());
         System.out.println(signUpForm.getPassword());
         System.out.println("*******************");
-    return signUpForm + "がAPIに渡されました";
+        String encodedPassword = passwordEncoder.encode(signUpForm.getPassword());
+    return encodedPassword + "がAPIに渡されました";
     }
 }
