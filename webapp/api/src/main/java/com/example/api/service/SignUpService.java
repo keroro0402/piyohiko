ca8d7c4068a64faf6cd7ad964f2bc45d400f8872
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-05-30 12:25:34 +0900

    SignUpService新規作成

---
+++ webapp/api/src/main/java/com/example/api/service/SignUpService.java
@@ -0,0 +1,11 @@
+package com.example.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SignUpService {
+    public String SignUp(){
+        System.out.println("ssasasaaaa");
+    return "SignUpが実行されました";
+    }
+}
