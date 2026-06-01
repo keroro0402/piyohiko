1c54de5b2a0e68fff7bec3674080c6d4dbbf2bda
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-06-01 09:57:38 +0900

    SECURITY_PHRASE用の入力フォーム追加

--- webapp/web/app/pages/register/index.vue
+++ webapp/web/app/pages/register/index.vue
@@ -9,6 +9,7 @@
         <FormGroupInput :id="FIELD.LOGIN_ID" v-model="loginId" v-bind="loginIdProps" :errors="errors" :block="BLOCK_NAME" :text="TEXT.FORM.LOGINID" placeholder="test" autocomplete="username" required />
         <FormGroupInput :id="FIELD.PASSWORD" v-model="password" v-bind="passwordProps" :errors="errors" :block="BLOCK_NAME" :text="TEXT.FORM.PASSWORD" type="password" minlength="1" placeholder="test" required />
         <FormGroupInput :id="FIELD.CONFIRM_PASSWORD" v-model="confirmPassword" v-bind="confirmPasswordProps" :errors="errors" :block="BLOCK_NAME" :text="TEXT.REGISTER.CONFIRM_PASSWORD_LABEL" type="password" minlength="1" placeholder="test" required />
+        <FormGroupInput :id="FIELD.SECURITY_PHRASE" v-model="securityPhrase" v-bind="securityPhraseProps" :errors="errors" :block="BLOCK_NAME" :text="TEXT.REGISTER.SECURITY_PHRASE_LABEL" type="password" minlength="1" placeholder="test" required />
         <SubmitButton :block="BLOCK_NAME" :is-form-valid="isFormValid" :text="TEXT.REGISTER.LABEL" />
         <div :class="`${BLOCK_NAME}__switch-page`">
           <NuxtLink :class="`${BLOCK_NAME}__switch-link`" :to="LINKS.TEXT.LOGIN">{{ TEXT.REGISTER.EXISTING_USER }}</NuxtLink>
@@ -48,6 +49,7 @@ const BLOCK_NAME = 'register-form';
 const FIELD = {
   LOGIN_ID: 'loginId',
   PASSWORD: 'password',
+  SECURITY_PHRASE: 'securityPhrase',
   CONFIRM_PASSWORD: 'confirmPassword',
 };
 
@@ -67,6 +69,7 @@ const {
     loginId: '',
     password: '',
     confirmPassword: '',
+    securityPhrase: '',
   },
 });
 const [
@@ -76,7 +79,7 @@ const [
 ] = defineField('loginId');
 const [password, passwordProps] = defineField('password');
 const [confirmPassword, confirmPasswordProps] = defineField('confirmPassword');
-
+const [securityPhrase, securityPhraseProps] = defineField('securityPhrase');
 /* 画面独自のリアクティブな状態（ref / computed） */
 const registerFailed = ref('');
 const isFormValid = computed(() => meta.value.valid && meta.value.dirty); // VeeValidateの結果がvalidにbooleanで入る
