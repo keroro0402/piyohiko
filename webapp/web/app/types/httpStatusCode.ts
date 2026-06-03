0b55aa4219011dea26454e70deaa84d010dc020e
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-06-04 00:13:27 +0900

    HttpStatusCodeの型をenumで定義

---
+++ webapp/web/app/types/httpStatusCode.ts
@@ -0,0 +1,11 @@
+/**
+ * HttpStatusCode定義
+ * enum で定義
+ */
+export enum HttpStatusCode {
+  BAD_REQUEST = 400,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  CONFLICT = 409,
+  INTERNAL_SERVER_ERROR = 500,
+}
