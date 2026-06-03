662325054e765a2ce00af6e837fb9cc92480a8ac
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-06-04 00:41:56 +0900

    トーストロジック作成

---
+++ webapp/web/app/composables/useToast.ts
@@ -0,0 +1,37 @@
+import { useToast as useLibToast } from 'vue-toastification';
+import { TEXT } from '~/constants/text';
+import { STATES_CODE } from '~/constants/api-status';
+import type { CustomAxiosError } from '~/types/customAxiosError';
+
+export const useToast = () => {
+  const toast = useLibToast();
+  const showSuccessToast = (message: string) => {
+    toast.success(message, {
+      timeout: false,
+      closeOnClick: true,
+    });
+  };
+
+  const showApiErrorToast = (error: CustomAxiosError) => {
+    const statusCode = error?.response?.status;
+    const errorMessage = error?.response?.data?.message?.[0];
+    if (statusCode === STATES_CODE.BAD_REQUEST) {
+      toast.error(errorMessage || TEXT.ERROR.ERROR400, {
+        timeout: false,
+        closeOnClick: true,
+      });
+    } else if (statusCode === STATES_CODE.CONFLICT) {
+      toast.error(errorMessage || TEXT.ERROR.ERROR409, {
+        timeout: false,
+        closeOnClick: true,
+      });
+    } else {
+      toast.error(TEXT.ERROR.ERROR500, {
+        timeout: false,
+        closeOnClick: true,
+      });
+    }
+  };
+
+  return { showSuccessToast, showApiErrorToast };
+};
