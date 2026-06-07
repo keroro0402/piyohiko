321be2c10a3c5b9dbb453935dc6d6542dd17bf68
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-06-08 01:31:58 +0900

    404用のtoastの条件を追加し、判別をifからswitchに変更

--- webapp/web/app/composables/useToast.ts
+++ webapp/web/app/composables/useToast.ts
@@ -16,25 +16,37 @@ export const useToast = () => {
   const showApiErrorToast = (error: CustomAxiosError) => {
     const statusCode = error?.response?.status;
     const errorMessage = error?.response?.data?.message?.[0];
-    if (statusCode === STATES_CODE.BAD_REQUEST) {
-      toast.error(errorMessage || TEXT.ERROR.ERROR400, {
-        timeout: false,
-        closeOnClick: true,
-        bodyClassName: 'pre-line-toast',
-      });
-    } else if (statusCode === STATES_CODE.CONFLICT) {
-      toast.error(errorMessage || TEXT.ERROR.ERROR409, {
-        timeout: false,
-        closeOnClick: true,
-        bodyClassName: 'pre-line-toast',
-      });
-    } else {
-      toast.error(TEXT.ERROR.ERROR500, {
-        timeout: false,
-        closeOnClick: true,
-        bodyClassName: 'pre-line-toast',
-      });
+
+    let defaultMessage: string = TEXT.ERROR.UNKNOWNERROR;
+
+    switch (statusCode) {
+      case STATES_CODE.BAD_REQUEST:
+        defaultMessage = TEXT.ERROR.ERROR400;
+        break;
+      case STATES_CODE.UNAUTHORIZED:
+        defaultMessage = TEXT.ERROR.ERROR401;
+        break;
+      case STATES_CODE.FORBIDDEN:
+        defaultMessage = TEXT.ERROR.ERROR403;
+        break;
+      case STATES_CODE.NOT_FOUND:
+        defaultMessage = TEXT.ERROR.ERROR404;
+        break;
+      case STATES_CODE.CONFLICT:
+        defaultMessage = TEXT.ERROR.ERROR409;
+        break;
+      case STATES_CODE.INTERNAL_SERVER_ERROR:
+        defaultMessage = TEXT.ERROR.ERROR500;
+        break;
+      default:
+        defaultMessage = TEXT.ERROR.UNKNOWNERROR;
     }
+    const finalMessage = errorMessage || defaultMessage;
+    toast.error(finalMessage, {
+      timeout: false,
+      closeOnClick: true,
+      bodyClassName: 'pre-line-toast',
+    });
   };
 
   return { showSuccessToast, showApiErrorToast };
