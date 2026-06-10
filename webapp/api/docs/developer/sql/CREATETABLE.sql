d26df9b73badd25f1e8fdc365b430cafba144226
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-06-10 14:30:58 +0900

    t_password_reset_token用のDDL作成

---
+++ webapp/api/docs/developer/sql/CREATETABLE.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `api_db`.`t_user` (
+    user_id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (user_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `api_db`.`t_password_reset_token` (
+    user_id INT NOT NULL,
+    token VARCHAR(36) NOT NULL,
+    expiry_date DATETIME NOT NULL,
+    is_used BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (token) , -- 同ユーザが複数回りクエストする可能性を考慮し、主キーはtoken
+    CONSTRAINT fk_password_reset_user_id FOREIGN KEY (user_id)
+        REFERENCES t_user(user_id)
+);
--- webapp/api/docs/developer/sql/CREATETABLE_t_user.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `api_db`.`t_user` (
-    user_id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (user_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
