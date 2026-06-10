98871dc7f4983374f35a27f9e531fa993aa0f616
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-06-10 15:19:49 +0900

    UserRepositoryにinsertPasswordResetTokenを実装

--- webapp/api/src/main/java/com/example/api/repository/UserRepository.java
+++ webapp/api/src/main/java/com/example/api/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.api.repository;
 
+import com.example.api.entity.PasswordResetToken;
 import com.example.api.entity.User;
 
 public interface UserRepository {
     User findByEmail(String email);
     void createUser(User user);
+    void insertPasswordResetToken(PasswordResetToken passwordResetToken);
     void save(User user);
 }
