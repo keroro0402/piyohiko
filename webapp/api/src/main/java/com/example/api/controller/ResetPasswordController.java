fe569e60be25a54ec9ec15ccaa9111d13d61bb10
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-06-11 16:14:16 +0900

    ResetPasswordControllerを新規作成

--- webapp/api/src/main/java/com/example/api/controller/ResetPasswordController.java
+++ webapp/api/src/main/java/com/example/api/controller/ResetPasswordController.java
@@ -1,20 +1,29 @@
 package com.example.api.controller;
 
+import com.example.api.dto.ResetPasswordRequestDto;
 import com.example.api.dto.ResetPasswordResponseDto;
 import com.example.api.form.ResetPasswordForm;
+import com.example.api.service.ResetPasswordService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class ResetPasswordController {
+
+    private final ResetPasswordService resetPasswordService;
+
     @PostMapping("/password-reset")
     public ResetPasswordResponseDto resetPassword(
             @RequestBody @Validated ResetPasswordForm resetPasswordForm
     ){
-        ResetPasswordResponseDto resetPasswordResponseDto = new ResetPasswordResponseDto();
-        resetPasswordResponseDto.setMessage("ここまで来た");
-        return resetPasswordResponseDto;
+        ResetPasswordRequestDto resetPasswordRequestDto = new ResetPasswordRequestDto();
+        resetPasswordRequestDto.setEmail(resetPasswordForm.getEmail());
+        resetPasswordRequestDto.setSecretCode(resetPasswordForm.getSecretCode());
+        resetPasswordRequestDto.setPassword(resetPasswordForm.getPassword());
+        return resetPasswordService.resetPassword(resetPasswordRequestDto);
     }
 }
