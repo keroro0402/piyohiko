d18e039b6a84b635af12b71933c0094a61be309c
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-06-12 15:05:46 +0900

    ResetPasswordServiceImplを新規作成

---
+++ webapp/api/src/main/java/com/example/api/service/ResetPasswordServiceImpl.java
@@ -0,0 +1,53 @@
+package com.example.api.service;
+
+import com.example.api.dto.ResetPasswordRequestDto;
+import com.example.api.dto.ResetPasswordResponseDto;
+import com.example.api.entity.ResetPassword;
+import com.example.api.entity.User;
+import com.example.api.exception.BusinessException;
+import com.example.api.repository.ResetPasswordRepository;
+import com.example.api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class ResetPasswordServiceImpl implements ResetPasswordService{
+
+    private final ResetPasswordRepository resetPasswordRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public ResetPasswordResponseDto resetPassword(ResetPasswordRequestDto resetPasswordRequestDto) {
+
+        String email = resetPasswordRequestDto.getEmail();
+        String secretCode = resetPasswordRequestDto.getSecretCode();
+        LocalDateTime localDateTime = java.time.LocalDateTime.now();
+
+        User user = userRepository.findByEmail(email);
+        if(user == null) {
+            throw new BusinessException(400, "USER_NOT_FOUND", "ユーザーが見つかりません");
+        }
+
+        ResetPassword resetPassword = resetPasswordRepository
+                .findByUserIdAndSecretCodeAndIsUsedFalseAndExpiryDateAfter(
+                        user.getUserId(),
+                        secretCode,
+                        LocalDateTime.now()
+                ).orElseThrow(() -> new BusinessException(400, "INVALID_CODE", "コードが無効または期限切れです"));
+
+
+        String encodedPassword = passwordEncoder.encode(resetPasswordRequestDto.getPassword()); // 新パスワードをエンコード
+        userRepository.passwordUpdate( user.getUserId(), encodedPassword); // t_user の password を更新
+        resetPasswordRepository.updateIsUsedToTrue(resetPassword.id());  // t_password_reset_request の is_used を更新
+
+        ResetPasswordResponseDto resetPasswordResponseDto = new ResetPasswordResponseDto();
+        resetPasswordResponseDto.setMessage("PW更新を完了しました");
+
+        return resetPasswordResponseDto;
+    }
+}
