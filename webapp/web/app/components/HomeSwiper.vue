f0ccdba928776d433bfcf0bb80523dc6689b9664
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-06-13 16:39:39 +0900

    Homeページ用Swiperを作成

---
+++ webapp/web/app/components/HomeSwiper.vue
@@ -0,0 +1,25 @@
+<template>
+  <div ref="swiperRef" class="swiper">
+    <div class="swiper-wrapper">
+      <div v-for="image in IMAGES.INDEX" :key="image.key" class="swiper-slide">
+        <img class="slide-img" :src="image.path" :alt="image.alt" :loading="image.loading" />
+      </div>
+    </div>
+    <div class="swiper-pagination" />
+    <div class="swiper-button-prev" />
+    <div class="swiper-button-next" />
+    <div class="swiper-scrollbar" />
+  </div>
+</template>
+<script setup lang="ts">
+import { IMAGES } from '~/constants/images';
+const { swiperRef } = useSwiper();
+</script>
+<style lang="scss" scoped>
+.swiper {
+  width: 70%;
+}
+.swiper-slide {
+  text-align: center;
+}
+</style>
