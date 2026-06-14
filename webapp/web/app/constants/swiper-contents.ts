13e176cbcdb688d8da63501b228b3f12d8f082c3
keroro0402 <otsuyu0402@yahoo.co.jp>
2026-06-14 15:53:00 +0900

    SWIPER_CONTENTSを新規作成

---
+++ webapp/web/app/constants/swiper-contents.ts
@@ -0,0 +1,24 @@
+import type { ContentItem } from '~/types/swiperContetnts';
+import { TEXT } from '~/constants/text';
+import { IMAGES } from '~/constants/images';
+/**
+ * swiperコンテンツ定義
+ *
+ * @rules
+ * - key は大文字
+ */
+export const SWIPER_CONTENTS: {
+  INDEX: readonly ContentItem[];
+} = {
+  INDEX: [
+    {
+      id: 0,
+      title: TEXT.SWIPER_CONTENTS.TITLE,
+      category: TEXT.SWIPER_CONTENTS.CATEGORY,
+      image: {
+        level: IMAGES.INDEX_DISCRIPTION[3],
+      },
+      description: TEXT.SWIPER_CONTENTS.DESCRIPTION,
+    },
+  ],
+} as const;
